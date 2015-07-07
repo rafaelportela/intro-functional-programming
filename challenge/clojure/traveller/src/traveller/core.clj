2c4e4f192ad8f8ceceae9b552ba7840a1c29b1db
Rafael Portela <rafaelportela90@gmail.com>
2015-07-07 20:10:23 -0300

    Traveller/clojure: convert bill value to rand.

--- challenge/clojure/traveller/src/traveller/core.clj
+++ challenge/clojure/traveller/src/traveller/core.clj
@@ -3,3 +3,15 @@
 (defn filter-bills
   [bills]
   (filter #(not= (:city %) "Johannesburg") bills))
+
+(defn convert
+  [rate value]
+  (* rate value))
+
+(defn select
+  [currency convertion-rates]
+  ((keyword currency) convertion-rates))
+
+(defn rands-value
+  [bill convertion-rates]
+  (Math/round (convert (:value bill) (select (:currency bill) convertion-rates))))
--- challenge/clojure/traveller/test/traveller/core_test.clj
+++ challenge/clojure/traveller/test/traveller/core_test.clj
@@ -5,3 +5,20 @@
 (deftest filter-za-receipts-out
   (testing "Filter receipts from za out."
     (is (= (filter-bills [{:city "Johannesburg"}]) []))))
+
+(deftest apply-convertion-to-value
+  (testing "Given currency convertion rate, it applies the convertion to value."
+    (let [convertion-rate 0.5 value 10]
+      (is (= (convert value convertion-rate) 5.0)))))
+
+(deftest select-convertion-rate
+  (testing "Select given convertion rate value"
+    (let [convertion-rates {:real 0.3}]
+      (is (= (select "real" convertion-rates) 0.3)))))
+
+(deftest select-convertion-rate-and-apply
+  (testing "select convertion rate base on currency name"
+    (let [
+          convertion-rates {:shilling 0.0003}
+          bill {:currency "shilling" :value 100000}]
+      (is (= (rands-value bill convertion-rates) 30)))))
